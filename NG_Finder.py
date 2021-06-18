cc795fc9741eda361972a935f2aa153f3cf860d6
kevalraval1 <62306998+kevalraval1@users.noreply.github.com>
2021-06-18 19:32:43 -0400

    Update 1: Removed position paramater
    Only FASTA sequence with mutation in brackets has to be put into the function, no bp number required. Error messages are accurate. Next update needs to fix returns and prints, make them all returns or all prints depending on requirements of the eventual program. Most likely will be return for success, print for error messages and exit.

--- NG_Finder.py
+++ NG_Finder.py
@@ -1,32 +1,55 @@
 # print ("Enter your FASTA Sequence: ")
 # FASTA = input()
-# print ("Enter the numeric position of mutation you want to induce: ")
-# position = input()
-# print (NG_Finder(FASTA, position))
+# NG_Finder(FASTA)
 
-def NG_Finder (FASTA, position):
+def NG_Finder (FASTA):
+    counter = 0
+    position = 0
+    new_string = ""
+    for char in FASTA:
+        if (char != "(") and (char != ")"):
+            new_string = new_string + char
+            counter += 1
+        elif (char == ")"):
+            continue
+        else:
+            position = counter + 1
+            continue
     optimal_string = ""
     answer = False
     listByPos = []
-    if len(FASTA) < 11:
+    if len(new_string) < 11:
         return print("ERROR: FASTA given is too small, please enter a FASTA that is 11 or more base pairs.")
-    if ((position - 7) < 0) or (position + 4 > len(FASTA)):
+    if ((position - 7) < 0) or (position + 4 > len(new_string)):
         return print("ERROR: Selected mutation site is out of bounds for editing in this FASTA file. Add more base pairs to the ends for editing.")
     for x in range (position-7, position+4):
-        optimal_string = optimal_string + FASTA[x]
+        optimal_string = optimal_string + new_string[x]
     counter = position - 7 
     print (optimal_string)
     for x in optimal_string:
         if x == "G":
             if (counter != position-7):
-                tuple = (counter, FASTA[counter-1]+"G")
+                tuple = (counter, new_string[counter-1]+"G")
                 listByPos.append(tuple)
                 answer = True
         counter += 1
     if answer == False:
         return print("No available NG PAM sites for given mutation.")
-    return listByPos
+    if answer == True:
+        return print(listByPos)
 
-FASTA = "ACTAGCTACGACTACGCATACGCATGCTCTATCATCATCTGTTAAATATAT"
-print (NG_Finder(FASTA,12))
+FASTA = "ACTAGCTACGA(C)TACGCATACGCATGCTCTATCATCATCTGTTAAATATAT"
+FASTA1 = "TAAATATAT"
+FASTA2 = "ACTAGCTACGACTACGCATACGCATGCTCTATCATCATCTGTTAAATA(T)AT"
+FASTA3 = "ACTA(G)CTACGACTACGCATACGCATGCTCTATCATCATCTGTTAAATATAT"
+FASTA4 = "AAAAAAAAAA(A)AAAAAAAAAAAAAAAAAAAAA"
+
+NG_Finder(FASTA)
+NG_Finder(FASTA1)
+NG_Finder(FASTA2)
+NG_Finder(FASTA3)
+NG_Finder(FASTA4)
+
+
+
 

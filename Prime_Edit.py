5bc7f965312a5a4f0ac4f9573a0cc1923c158cd6
Keval Raval <Keval@Kevals-MacBook-Pro.local>
2021-07-30 19:31:29 -0400

    Made a new file for the interface to set up for Base Editing Tool

--- Prime_Edit.py
+++ Prime_Edit.py
@@ -1,9 +1,6 @@
 from tkinter import *
 import sys, os, regex as re
 
-window = Tk()
-window.title("Single Base Editing Helper Tool")
-
 def reverser(FASTA, mutation):
     global newFASTA
     global newMutation
@@ -205,13 +202,9 @@ def analysisPrinter(listByPos, listOfSpacers, listOfExtensions, file1):
     addString = "\n".join(printList)
     file1.write(addString)
 
-def main():
-    FASTA = FASTAEntry.get()
-    mutation = mutationEntry.get()
-    filename = filenameEntry.get()
-    inputPAM = pamEntry.get()
+def main(FASTA, mutation, filename, inputPAM, PBS):
     global PBSlength
-    PBSlength = int(PBSlengthEntry.get())
+    PBSlength = int(PBS)
     #For testing code:
     completename = os.path.join(os.path.dirname("Prime_Edit.py"), (filename + ".txt"))
     #For testing executable:
@@ -241,45 +234,3 @@ def main():
     sys.exit()
 
 # FASTA = "ACCATGCTCTATCATCATCTCATGCTCTATCATCATCTCATGCTCTATCATCATCTCATGCTGTATCATCATCTTAGCGACGGT(G)TAGCATGCTCTATCATCATCTCATGCTCTATCATCATCTGCATACGCATGCTCTATCATCATCTGTTAAATATAT"
-
-canvas = Canvas(window, height = 200, width = 600)
-canvas.pack()
-
-frame = Frame(window,relief = 'groove')
-frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.8)
-
-welcome = Label(frame, text = "Welcome to the Single Base Editing Helper Tool", fg = "Black")
-welcome.pack(side = "top")
-
-options = ["--Select tool to use--", "Prime Editing Tool", "Base Editing Tool"]
-
-clicked = StringVar()
-clicked.set(options[0])
-
-drop = OptionMenu(window, clicked, *options)
-drop.pack(side = "top")
-
-FASTAEntry = Entry(frame, width = 50)
-FASTAEntry.pack(side = "top")
-FASTAEntry.insert(0, "Please enter the DNA sequence")
-
-mutationEntry = Entry(frame, width = 50)
-mutationEntry.pack(side = "top")
-mutationEntry.insert(0, "Please enter the desired mutation")
-
-pamEntry = Entry(frame, width = 50)
-pamEntry.pack(side = "top")
-pamEntry.insert(0, "Please enter the desired PAM sequence if applicable")
-
-PBSlengthEntry = Entry(frame, width = 50)
-PBSlengthEntry.pack(side = "top")
-PBSlengthEntry.insert(0, "Please enter PBS Length (from 7-17) if applicable")
-
-filenameEntry = Entry(frame, width = 50)
-filenameEntry.pack(side = "top")
-filenameEntry.insert(0, "Please enter the desired .txt filename")
-
-enterButton = Button(window, text = "Start", padx = 10, pady = 5, fg = "Black", bg = "gray", command = main)
-enterButton.pack()
-
-window.mainloop()
---
+++ main_interface.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import Prime_Edit as PE
+
+window = Tk()
+window.title("Single Base Editing Helper Tool")
+
+def main():
+    if clicked.get() == "Prime Editing Tool":
+        PE.main(FASTAEntry.get(), mutationEntry.get(), filenameEntry.get(), pamEntry.get(), PBSlengthEntry.get())
+
+    elif clicked.get() == "Base Editing Tool":
+        pass
+
+
+canvas = Canvas(window, height = 200, width = 600)
+canvas.pack()
+
+frame = Frame(window,relief = 'groove')
+frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.8)
+
+welcome = Label(frame, text = "Welcome to the Single Base Editing Helper Tool", fg = "Black")
+welcome.pack(side = "top")
+
+options = ["--Select tool to use--", "Prime Editing Tool", "Base Editing Tool"]
+
+clicked = StringVar()
+clicked.set(options[0])
+drop = OptionMenu(window, clicked, *options)
+drop.pack(side = "top")
+
+FASTAEntry = Entry(frame, width = 50)
+FASTAEntry.pack(side = "top")
+FASTAEntry.insert(0, "Please enter the DNA sequence")
+
+mutationEntry = Entry(frame, width = 50)
+mutationEntry.pack(side = "top")
+mutationEntry.insert(0, "Please enter the desired mutation")
+
+pamEntry = Entry(frame, width = 50)
+pamEntry.pack(side = "top")
+pamEntry.insert(0, "Please enter the desired PAM sequence if applicable")
+
+PBSlengthEntry = Entry(frame, width = 50)
+PBSlengthEntry.pack(side = "top")
+PBSlengthEntry.insert(0, "Please enter PBS Length (from 7-17) if applicable")
+
+filenameEntry = Entry(frame, width = 50)
+filenameEntry.pack(side = "top")
+filenameEntry.insert(0, "Please enter the desired .txt filename")
+
+enterButton = Button(window, text = "Start", padx = 10, pady = 5, fg = "Black", bg = "gray", command = main)
+enterButton.pack()
+
+window.mainloop()
